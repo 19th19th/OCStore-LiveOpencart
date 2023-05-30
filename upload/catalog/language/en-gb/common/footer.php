64477acb7804e587a115fb0a63176af307afa4f4
unishop2 <50132185+unishop2@users.noreply.github.com>
2023-05-30 16:00:00 +0400

    Revert "Merge branch 'main' of https://github.com/19th19th/OCStore-LiveOpencart"
    This reverts commit 4114b8796693ca78e03254e684822e36d02c5186, reversing
    changes made to 50503c4b8ef8e4b7ecb222f71dfe7a38c52e62a9.

--- upload/catalog/language/en-gb/common/footer.php
+++ upload/catalog/language/en-gb/common/footer.php
@@ -17,4 +17,4 @@ $_['text_account']      = 'My Account';
 $_['text_order']        = 'Order History';
 $_['text_wishlist']     = 'Wish List';
 $_['text_newsletter']   = 'Newsletter';
-$_['text_powered']      = 'Powered By <a target="_blank" href="https://liveopencart.ru/?utm_source=ocstore3_footer">OCStore LiveOpencart</a><br /> %s &copy; %s';
+$_['text_powered']      = 'Powered By <a href="https://ocstore.com/?utm_source=ocstore3_install">ocStore</a><br /> %s &copy; %s';
