7dcca0d5f5888a03ccf44bdde04bf321b14cdb1b
19th <19th19th@gmail.com>
2024-02-15 10:18:33 +0600

    Просьба не использовать редактор шаблонов

--- upload/admin/language/en-gb/design/theme.php
+++ upload/admin/language/en-gb/design/theme.php
@@ -9,7 +9,7 @@ $_['text_store']        = 'Choose your store';
 $_['text_template']     = 'Choose a template';
 $_['text_default']      = 'Default';
 $_['text_history']      = 'Theme History';
-$_['text_twig']         = 'The theme editor uses the template language Twig. You can read about <a href="http://twig.sensiolabs.org/documentation" target="_blank" class="alert-link">Twig syntax here</a>.';
+$_['text_twig'] 		= 'We strongly recommend against using the template editor, <a href="https://liveopencart.ru/docs-opencart-3/03-design/redaktor-shablona/"target="_blank" class="alert-link">more details</a>.';
 
 // Column
 $_['column_store']      = 'Store';
