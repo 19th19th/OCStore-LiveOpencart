17660b55db3136dd09c02609acf8f714db048ccd
19th <19th19th@gmail.com>
2024-07-21 18:37:15 +0600

    Исправление главной категории в копируемом товаре
    https://forum.liveopencart.ru/viewtopic.php?p=4547#p4547

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -378,6 +378,7 @@ class ModelCatalogProduct extends Model {
 			$data['product_related_article'] = $this->getArticleRelated($product_id);
 			$data['product_reward'] = $this->getProductRewards($product_id);
 			$data['product_special'] = $this->getProductSpecials($product_id);
+			$data['main_category_id'] = $this->getProductMainCategoryId($product_id);
 			$data['product_category'] = $this->getProductCategories($product_id);
 			$data['product_download'] = $this->getProductDownloads($product_id);
 			$data['product_layout'] = $this->getProductLayouts($product_id);
