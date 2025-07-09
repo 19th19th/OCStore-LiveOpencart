d24d19fd720a84d72bb838edc76c6df2ac23db99
19th <19th19th@gmail.com>
2025-07-09 18:30:51 +0600

    Вывод версий PHP и IonCube в админке

--- upload/admin/controller/common/footer.php
+++ upload/admin/controller/common/footer.php
@@ -5,8 +5,14 @@ class ControllerCommonFooter extends Controller {
 
 		if ($this->user->isLogged() && isset($this->request->get['user_token']) && ($this->request->get['user_token'] == $this->session->data['user_token'])) {
 			$data['text_version'] = sprintf($this->language->get('text_version'), VERSION);
+
+			$php_version = phpversion();
+			$ioncube_version = function_exists('ioncube_loader_version') ? ioncube_loader_version() : 'Not installed';
+
+			$data['extra_version'] = ' | PHP ' . $php_version . ' | IonCube ' . $ioncube_version;
 		} else {
 			$data['text_version'] = '';
+			$data['extra_version'] = '';
 		}
 
 		return $this->load->view('common/footer', $data);
